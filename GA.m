deaba474cfa340b12e6485b53a55e0da725da83a
masamalisic <masamalisicc@gmail.com>
2025-10-26 18:10:08 +0100

    Modified GA code for optimization of five different functions

--- GA.m
+++ GA.m
@@ -1,88 +1,91 @@
 clc; clear all; close all;
 
-syms x y n;
-syms i_d i_g;
+func_name = 'Rosenbrock';
+[lb, ub, dim, fobj] = get_function_details(func_name);
 
 n = 100;
 n_elite = 5;
 n_crossover = 90; 
 n_mutation = 2;
 
-[population] = coding( -5, 5, n );
+[population] = coding(lb, ub, n);
 
 vec = [];
 
 for i = 1:10
-    
-    [minimal_value] = Rosenbrock( population, n, n_elite, n_crossover, n_mutation );
-
-    if minimal_value < -5
-        greska = 'MB';
-    end
-
+    [minimal_value] = GA_function(population, n, n_elite, n_crossover, n_mutation, fobj, lb, ub);
     vec = [vec; minimal_value];
 end
 
-vec
+minimalno = min(vec);
+maksimalno = max(vec);
+srednja_vr = mean(vec);
+sd = std(vec);
+
+fprintf('\n*** Statistika za funkciju %s ***\n', func_name);
+fprintf('Minimalna vrednost: %.6f\n', minimalno);
+fprintf('Maksimalna vrednost: %.6f\n', maksimalno);
+fprintf('Srednja vrednost:   %.6f\n', srednja_vr);
+fprintf('Standardna devijacija: %.6f\n', sd);
+
+disp(vec)
 
 figure(1)
 plot(vec)
 yline(0, 'r--') 
+title(['Optimizacija funkcije: ', func_name])
+xlabel('Iteracija')
+ylabel('Minimalna vrednost')
 hold on;
+
 figure(2)
-boxplot( vec )
+boxplot(vec)
+title(['Distribucija minimalnih vrednosti - ', func_name])
 
-function z = fun(x, y)
-    z = ( x - 1 ).^2 + 100 * ( y - x.^2 ).^2;
-end
-
-
-function[ population ] = coding( i_d, i_g, n ) 
-    
+function [population] = coding(lb, ub, n)
+    dim = length(lb);
+    population = zeros(n, dim);
     for i = 1:n
-        for j = 1:2
-            population(i,j) = i_d + ( i_g - i_d ) * rand();
+        for j = 1:dim
+            population(i,j) = lb(j) + (ub(j) - lb(j)) * rand();
         end
     end
 end
+ 
 
-
-function[ selected_individuals, elite_individuals ] = selection( population, n, number_of_elite ) 
+function [selected_individuals, elite_individuals] = selection(population, n, number_of_elite, fobj)
 
    fitness_values = zeros(1,n);
    probability = zeros(1,n); 
    cumulative_sum = zeros(n,2); 
-   sum = 0;
+   sum_val = 0;
 
    for i = 1:n
-        fitness_values(i) = fun( population(i,1), population(i,2));
+        fitness_values(i) = fobj(population(i,:));
         fitness_values(i) = 1 / fitness_values(i);   
-        sum = sum + fitness_values(i);
+        sum_val = sum_val + fitness_values(i);
    end
 
    for i = 1:n
-        probability(i) = fitness_values(i)  / sum;
+        probability(i) = fitness_values(i)  / sum_val;
    end
 
-   probability = round( probability, 3 );
+   probability = round(probability, 3);
 
-   elite_individuals = zeros(number_of_elite,2);
+   elite_individuals = zeros(number_of_elite, 2);
 
-   [ ~, idx ] = sort( fitness_values, "descend" );
+   [~, idx] = sort(fitness_values, "descend");
    
    for i = 1:number_of_elite 
-        
-       elite_individuals(i,:) = population( idx(i), : ); 
-       
+       elite_individuals(i,:) = population(idx(i), :); 
    end
  
    new_size = n - number_of_elite;
-
    cumulative_sum(:,1) = cumsum(probability);
        
    for i = 1:new_size 
       random_num = rand();
-      index = find( cumulative_sum(:,1) >= random_num, 1 );
+      index = find(cumulative_sum(:,1) >= random_num, 1);
        if ~isempty(index)
         cumulative_sum(index, 2) = cumulative_sum(index, 2) + 1;   
        end
@@ -92,7 +95,6 @@ function[ selected_individuals, elite_individuals ] = selection( population, n, 
 
   for i = 1:n
     counter = cumulative_sum(i, 2);
-    
     if counter ~= 0
         for j = 1:counter
             selected_individuals(current_index, :) = population(i, :);
@@ -100,36 +102,31 @@ function[ selected_individuals, elite_individuals ] = selection( population, n, 
         end   
     end
   end
-
 end
 
-function[ selected_individuals ] = crossover( selected_individuals, n, n_crossover, population )
 
-    num_of_crossovered = round( n * ( n_crossover / 100 ) );
+function [selected_individuals] = crossover(selected_individuals, n, n_crossover, lb, ub)
+    num_of_crossovered = round(n * (n_crossover / 100));
 
     for i = 1:2:num_of_crossovered - 2
-    
         crossover_point = randi([1, 2]);
-         
         temp = selected_individuals(i, crossover_point);
         selected_individuals(i, crossover_point) = selected_individuals(i + 1, crossover_point);
         selected_individuals(i + 1, crossover_point) = temp;
-        
     end
     
-    [other_individuals] = coding(-5,5, n - num_of_crossovered);
+    [other_individuals] = coding(lb, ub, n - num_of_crossovered);
 
     j = 1;
     for i = num_of_crossovered+1:n
-        selected_individuals( i, : ) = other_individuals( j, : );
+        selected_individuals(i,:) = other_individuals(j,:);
         j = j + 1;
     end
-
 end
 
-function[ mutated_individuals  ] = mutation( crossovered_individuals, n, M ) 
-    
-    permutations = randperm(n, M); 
+
+function [mutated_individuals] = mutation(crossovered_individuals, n, M)
+    permutations = randperm(n, M);
     randoms = permutations(1:2);
 
     for i = 1:M 
@@ -138,29 +135,26 @@ function[ mutated_individuals  ] = mutation( crossovered_individuals, n, M )
     end
 
     mutated_individuals = crossovered_individuals;
-
 end
 
-function[ min_value ] = Rosenbrock( population, n, n_elite, n_crossover, n_mutation )
-    
+
+function [min_value] = GA_function(population, n, n_elite, n_crossover, n_mutation, fobj, lb, ub)
     counter = 1;
     
-    while counter <= 100000
-            
-        [ selected_individuals, elite_individuals ] = selection( population, n, n_elite );
+    while counter <= 100
+        [selected_individuals, elite_individuals] = selection(population, n, n_elite, fobj);
         
         new_n = n - n_elite;
-            
-        [ crossovered_individuals ] = crossover( selected_individuals, new_n, n_crossover, population );
-        [ mutated_individuals ] = mutation( crossovered_individuals, new_n, n_mutation );
+        [crossovered_individuals] = crossover(selected_individuals, new_n, n_crossover, lb, ub);
+        [mutated_individuals] = mutation(crossovered_individuals, new_n, n_mutation);
            
         population = vertcat(mutated_individuals, elite_individuals);
 
         for i = 1:n
-            fitness_values(i) = fun( population(i,1), population(i,2));
+            fitness_values(i) = fobj(population(i,:));
         end
          
-        min_value = min( fitness_values );
+        min_value = min(fitness_values);
         minimalne(counter) = min_value;
 
         counter = counter + 1;
