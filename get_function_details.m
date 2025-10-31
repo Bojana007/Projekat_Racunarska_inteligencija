4ee9eaf94c16b237c52f522760dac38f297fdf8d
Bojana Stambolic <stambolicbojana999@gmail.com>
2025-10-31 03:34:33 +0100

    Function file updated

--- get_function_details.m
+++ get_function_details.m
@@ -1,10 +1,4 @@
 function [lb, ub, dim, fobj] = get_function_details(func_name)
-% Ova funkcija vraća detalje o funkciji koju optimizujemo
-% lb - donje granice
-% ub - gornje granice
-% dim - broj promenljivih
-% fobj - function handle (funkcija koju treba minimizovati)
-
 switch func_name
     case 'Rosenbrock'
         fobj = @(x) (x(1)-1).^2 + 100*(x(2)-x(1).^2).^2;
@@ -18,20 +12,7 @@ switch func_name
         ub = [5.12, 5.12];
         dim = 2;
 
-    case 'Himmelblau'
-        fobj = @(x) (x(1).^2 + x(2) - 11).^2 + (x(1) + x(2).^2 - 7).^2;
-        lb = [-6, -6];
-        ub = [6, 6];
-        dim = 2;
-
-    case 'Sphere'
-        fobj = @(x) sum(x.^2);
-        lb = [-5, -5];
-        ub = [5, 5];
-        dim = 2;
-
     case 'Schwefel'
-        % Schwefel funkcija (globalni minimum oko [420.9687, 420.9687])
         fobj = @(x) 418.9829*length(x) - sum(x .* sin(sqrt(abs(x))));
         lb = [-500, -500];
         ub = [500, 500];
